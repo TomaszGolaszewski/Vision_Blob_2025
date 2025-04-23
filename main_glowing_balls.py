680b57650df1f740c01ef444d67bb7b9fb811ce2
Tomasz Golaszewski <tg@myodoo.pl>
2025-04-23 16:38:23 +0200

    Commit v0.4

--- main_glowing_balls.py
+++ main_glowing_balls.py
@@ -4,12 +4,14 @@ import time
 
 from my_vision_lib.miscellaneous import get_objects_by_color
 from my_vision_lib.blob import draw_blob
+from my_vision_lib.statistics import PointStabilized
 
 
 def main():
 
     canva_scale = 3
     object_area_size = 2000
+    list_with_blob_objects = []
 
     # capturing video through webcam
     webcam = cv2.VideoCapture(0)
@@ -31,8 +33,24 @@ def main():
         # detecting objects
         image_processed, objects_found = get_objects_by_color(image_original_frame, object_area_size)
 
+
+        # stabilization of objects coordinates
+        for blob in list_with_blob_objects:
+            blob.pick_point_from_list(objects_found)
+        # remove dead points
+        list_with_blob_objects = list(filter(lambda x: x.health, list_with_blob_objects))
+        # creation of new blob objects
+        if len(objects_found):
+            for new_object in objects_found:
+                list_with_blob_objects.append(PointStabilized(new_object))
+
+        # prepare new list of objects coordinates
+        stabilized_objects_found = [blob.coord for blob in list_with_blob_objects]
+        print(len(list_with_blob_objects), len(stabilized_objects_found)) # TODO: to remove
+
+
         # scaling down, creatingcanva and scaling up - performance optimization
-        objects_found_scaled = [(obj[0] // canva_scale, obj[1] // canva_scale) for obj in objects_found]
+        objects_found_scaled = [(obj[0] // canva_scale, obj[1] // canva_scale) for obj in stabilized_objects_found]
         image_blob = draw_blob(objects_found_scaled, image_width // canva_scale, image_height // canva_scale)
         image_resized = cv2.resize(image_blob, (image_width, image_height), interpolation=cv2.INTER_LINEAR)
 
